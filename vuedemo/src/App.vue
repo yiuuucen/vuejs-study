b5a2fcb75ed3a2d3779c8eafc1b2bcb8b7f15d3b
yiuuucen <1784373887@qq.com>
2017-12-22 19:06:56 +0800

    work

---
+++ vuedemo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <slider animation="fade">
+     <slider-item v-for="(i, index) in list" :key="index">
+      <div :style="i">
+        <p style="line-height: 280px; font-size: 5rem; text-align: center;">Page{{ index + 1 }}</p>
+      </div>
+     </slider-item>
+    </slider>
+  </div>
+</template>
+
+<script>
+import { Slider, SliderItem } from 'vue-easy-slider'
+export default {
+  data () {
+    return {
+      list: [
+        { backgroundColor: '#3f51b5', width: '100%', height: '100%' },
+        { backgroundColor: '#eee', width: '100%', height: '100%' },
+        { backgroundColor: '#f44336', width: '100%', height: '100%' },
+      ],
+    }
+  },
+  components: {
+    Slider,
+    SliderItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
