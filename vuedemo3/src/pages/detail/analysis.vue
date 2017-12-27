93e403a1c32456497745109e1b2942323e9211c8
yiuuucen <1784373887@qq.com>
2017-12-27 22:52:09 +0800

    work

--- vuedemo3/src/pages/detail/analysis.vue
+++ vuedemo3/src/pages/detail/analysis.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <div class="sales-board">
+  <div class="sales-board">
       <div class="sales-board-intro">
         <h2>流量分析</h2>
         <p>是指在获得网站访问量基本数据的情况下对有关数据进行统计、分析，从中发现用户访问网站的规律，并将这些规律与网络营销策略等相结合，从而发现目前网络营销活动中可能存在的问题，并为进一步修正或重新制定网络营销策略提供依据。当然这样的定义是站在网络营销管理的角度来考虑的</p>
@@ -10,7 +10,7 @@
                   购买数量：
               </div>
               <div class="sales-board-line-right">
-                <v-counter @on-change="onParamChange('buyNum', $event)"></v-counter>
+                
               </div>
           </div>
           <div class="sales-board-line">
@@ -18,7 +18,7 @@
                   产品类型：
               </div>
               <div class="sales-board-line-right">
-                  <v-selection :selections="buyTypes" @on-change="onParamChange('buyType', $event)"></v-selection>
+                  <v-selection :selections="buyTypes"></v-selection>
               </div>
           </div>
           <div class="sales-board-line">
@@ -26,9 +26,7 @@
                   有效时间：
               </div>
               <div class="sales-board-line-right">
-                  <v-chooser
-                  :selections="periodList"
-                  @on-change="onParamChange('period', $event)"></v-chooser>
+                  
               </div>
           </div>
           <div class="sales-board-line">
@@ -36,9 +34,7 @@
                   产品版本：
               </div>
               <div class="sales-board-line-right">
-                  <v-mul-chooser
-                  :selections="versionList"
-                  @on-change="onParamChange('versions', $event)"></v-mul-chooser>
+                  
               </div>
           </div>
           <div class="sales-board-line">
@@ -100,20 +96,93 @@
           </tr>
         </table>
         <h3 class="buy-dialog-title">请选择银行</h3>
-        <bank-chooser @on-change="onChangeBanks"></bank-chooser>
-        <div class="button buy-dialog-btn" @click="confirmBuy">
+        <!-- <bank-chooser ></bank-chooser> -->
+        <div class="button buy-dialog-btn" >
           确认购买
         </div>
       </my-dialog>
       <my-dialog :is-show="isShowErrDialog" @on-close="hideErrDialog">
         支付失败！
       </my-dialog>
-      <check-order :is-show-check-dialog="isShowCheckOrder" :order-id="orderId" @on-close-check-dialog="hideCheckOrder"></check-order>
-  </div> -->
+  </div>
 </template>
 
 <script>
-
+import MyDialog from '../../components/dialog'
+import VSelection from '../../components/selection'
+export default {
+  components:{
+    VSelection,
+    MyDialog
+  },
+  data () {
+    return {
+      buyNum: 0,
+      buyType: {},
+      versions: [],
+      period: {},
+      price: 0,
+      versionList: [
+        {
+          label: '客户版',
+          value: 0
+        },
+        {
+          label: '代理商版',
+          value: 1
+        },
+        {
+          label: '专家版',
+          value: 2
+        }
+      ],
+      periodList: [
+        {
+          label: '半年',
+          value: 0
+        },
+        {
+          label: '一年',
+          value: 1
+        },
+        {
+          label: '三年',
+          value: 2
+        }
+      ],
+      buyTypes: [
+        {
+          label: '入门版',
+          value: 0
+        },
+        {
+          label: '中级版',
+          value: 1
+        },
+        {
+          label: '高级版',
+          value: 2
+        }
+      ],
+      isShowPayDialog: false,
+      bankId: null,
+      orderId: null,
+      isShowCheckOrder: false,
+      isShowErrDialog: false
+    }
+  },
+  methods :{
+    showPayDialog(){
+      this.isShowPayDialog=true
+    },
+    hidePayDialog(){
+      this.isShowPayDialog=false
+    },
+    hideErrDialog(){
+      this.isShowPayDialog=false
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
